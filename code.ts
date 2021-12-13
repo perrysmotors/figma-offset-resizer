3444d65aca3bd0fe8101f04cb5b5a5f515e21fd2
nick <wrl.snk@gmail.com>
2021-12-14 01:44:52 +0300

    bounding box update. rotate and skew!

--- code.ts
+++ code.ts
@@ -1,5 +1,3 @@
-
-
 // This plugin resizes frames or component to a fixed offset from their contents.
 
 // The 'input' event listens for text change in the Quick Actions box after a plugin is 'Tabbed' into.
@@ -37,6 +35,7 @@ figma.on("run", ({ parameters }: RunEvent) => {
 
 function startPluginWithParameters(parameters: ParameterValues) {
     const selection = getFilteredSelection()
+
     if (selection.length === 0) {
         figma.notify("⚠️ Select at least one frame or component first")
         figma.closePlugin()
@@ -78,27 +77,18 @@ function resizeWithOffset(parent, offset, offsetHor) {
     if (children.length === 0) return
 
     if (parent.layoutMode === "NONE") {
-        // Calculate bounding box
-        const topLeftX = Math.min(...children.map((child) => child.x))
-        const topLeftY = Math.min(...children.map((child) => child.y))
-        const bottomRigthX = Math.max(
-            ...children.map((child) => child.x + child.width)
-        )
-        const bottomRigthY = Math.max(
-            ...children.map((child) => child.y + child.height)
-        )
-        const width = bottomRigthX - topLeftX
-        const height = bottomRigthY - topLeftY
+
+        let bounds = getBoundingRect(children)
 
         // Move and resize parent
-        parent.x = parent.x + topLeftX - offsetHor
-        parent.y = parent.y + topLeftY - offset
-        parent.resizeWithoutConstraints(width + offsetHor * 2, height + offset * 2)
+        parent.x = parent.x + bounds.x - offsetHor
+        parent.y = parent.y + bounds.y - offset
+        parent.resizeWithoutConstraints(bounds.width + offsetHor * 2, bounds.height + offset * 2)
 
         // Children move with parent, so they need to be moved back
         children.forEach((child) => {
-            child.x = child.x - topLeftX + offsetHor
-            child.y = child.y - topLeftY + offset
+            child.x = child.x - bounds.x + offsetHor
+            child.y = child.y - bounds.y + offset
         })
     } else {
         // If we have auto-layout
@@ -109,4 +99,171 @@ function resizeWithOffset(parent, offset, offsetHor) {
         parent.paddingTop = offset
         parent.paddingBottom = offset
     }
+}
+
+function applyMatrixToPoint(matrix: number[][], point: number[]) {
+    return [
+        point[0] * matrix[0][0] + point[1] * matrix[0][1] + matrix[0][2],
+        point[0] * matrix[1][0] + point[1] * matrix[1][1] + matrix[1][2]
+    ]
+}
+
+function getBoundingRect(nodes: SceneNode[]) {
+    const boundingRect = {
+        x: 0,
+        y: 0,
+        x2: 0,
+        y2: 0,
+        height: 0,
+        width: 0
+    }
+
+    if (nodes.length) {
+        const xy = nodes.reduce(
+            (rez, node) => {
+                if (!node.relativeTransform) {
+                    console.warn(
+                        'Provided node haven\'t "relativeTransform" property, but it\'s required for calculations.'
+                    )
+                    return rez
+                }
+                if (node.height === undefined || node.width === undefined) {
+                    console.warn(
+                        'Provided node haven\'t "width/height" property, but it\'s required for calculations.'
+                    )
+                    return rez
+                }
+                const halfHeight = node.height / 2
+                const halfWidth = node.width / 2
+
+                const [[c0, s0, x], [s1, c1, y]] = node.relativeTransform
+                const matrix = [
+                    [c0, s0, x + halfWidth * c0 + halfHeight * s0],
+                    [s1, c1, y + halfWidth * s1 + halfHeight * c1]
+                ]
+
+                // the coordinates of the corners of the rectangle
+                const XY = {
+                    x: [1, -1, 1, -1],
+                    y: [1, -1, -1, 1]
+                }
+
+                // fill in
+                for (let i = 0; i <= 3; i++) {
+                    const a = applyMatrixToPoint(matrix, [
+                        XY.x[i] * halfWidth,
+                        XY.y[i] * halfHeight
+                    ])
+                    XY.x[i] = a[0]
+                    XY.y[i] = a[1]
+                }
+
+                XY.x.sort((a, b) => a - b)
+                XY.y.sort((a, b) => a - b)
+
+                rez.x.push(XY.x[0])
+                rez.y.push(XY.y[0])
+                rez.x2.push(XY.x[3])
+                rez.y2.push(XY.y[3])
+                return rez
+            },
+            { x: [], y: [], x2: [], y2: [] }
+        )
+
+        const rect = {
+            x: Math.min(...xy.x),
+            y: Math.min(...xy.y),
+            x2: Math.max(...xy.x2),
+            y2: Math.max(...xy.y2)
+        }
+
+        boundingRect.x = rect.x
+        boundingRect.y = rect.y
+        boundingRect.x2 = rect.x2
+        boundingRect.y2 = rect.y2
+        boundingRect.width = rect.x2 - rect.x
+        boundingRect.height = rect.y2 - rect.y
+    }
+
+    return boundingRect
+}
+
+function getBoundingRect2(nodes: SceneNode[]) {
+    const boundingRect = {
+        x: 0,
+        y: 0,
+        x2: 0,
+        y2: 0,
+        height: 0,
+        width: 0
+    }
+
+    if (nodes.length) {
+        const xy = nodes.reduce(
+            (rez, node) => {
+                if (!node.relativeTransform) {
+                    console.warn(
+                        'Provided node haven\'t "relativeTransform" property, but it\'s required for calculations.'
+                    )
+                    return rez
+                }
+                if (node.height === undefined || node.width === undefined) {
+                    console.warn(
+                        'Provided node haven\'t "width/height" property, but it\'s required for calculations.'
+                    )
+                    return rez
+                }
+                const halfHeight = node.height / 2
+                const halfWidth = node.width / 2
+
+                const [[c0, s0, x], [s1, c1, y]] = node.relativeTransform
+                const matrix = [
+                    [c0, s0, x + halfWidth * c0 + halfHeight * s0],
+                    [s1, c1, y + halfWidth * s1 + halfHeight * c1]
+                ]
+
+                // the coordinates of the corners of the rectangle
+                const XY = {
+                    x: [1, -1, 1, -1],
+                    y: [1, -1, -1, 1]
+                }
+
+                // fill in
+                for (let i = 0; i <= 3; i++) {
+                    const a = applyMatrixToPoint(matrix, [
+                        XY.x[i] * halfWidth,
+                        XY.y[i] * halfHeight
+                    ])
+                    XY.x[i] = a[0]
+                    XY.y[i] = a[1]
+                }
+
+                XY.x.sort((a, b) => a - b)
+                XY.y.sort((a, b) => a - b)
+
+                rez.x.push(XY.x[0])
+                rez.y.push(XY.y[0])
+                rez.x2.push(XY.x[3])
+                rez.y2.push(XY.y[3])
+                return rez
+            },
+            { x: [], y: [], x2: [], y2: [] }
+        )
+
+        const rect = {
+            x: Math.min(...xy.x),
+            y: Math.min(...xy.y),
+            x2: Math.max(...xy.x2),
+            y2: Math.max(...xy.y2)
+        }
+
+        boundingRect.x = rect.x
+        boundingRect.y = rect.y
+        boundingRect.x2 = rect.x2
+        boundingRect.y2 = rect.y2
+        boundingRect.width = rect.x2 - rect.x
+        boundingRect.height = rect.y2 - rect.y
+    }
+
+    return boundingRect
 }
